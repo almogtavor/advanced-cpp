cec86cd9e859c4879f1dfb79c2e797a00ee05c0c
Almog Tavor <almogtavor@gmail.com>
2026-09-01 01:15:15 +0300

    feat: implement MappingAlgorithmImpl and register it in the global scope

--- ex_3/Algorithm/src/MappingAlgorithmImpl.cpp
+++ ex_3/Algorithm/src/MappingAlgorithmImpl.cpp
@@ -3,8 +3,11 @@
 
 namespace algorithm_323084962_212223036 {
 
-// TODO: implement MappingAlgorithmImpl_323084962_212223036 : common::IMappingAlgorithm
-// and register it in the global scope below with:
-//     REGISTER_MAPPING_ALGORITHM(algorithm_323084962_212223036::MappingAlgorithmImpl_323084962_212223036);
+class MappingAlgorithmImpl_323084962_212223036 : public common::IMappingAlgorithm {
+    // ctor forwarding to the IMappingAlgorithm base + nextStep override
+};
 
-} // namespace algorithm_323084962_212223036
+} // namespace
+
+using algorithm_323084962_212223036::MappingAlgorithmImpl_323084962_212223036;
+REGISTER_MAPPING_ALGORITHM(MappingAlgorithmImpl_323084962_212223036);
